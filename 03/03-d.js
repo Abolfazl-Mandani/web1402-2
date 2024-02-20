0e67e75ca09196b8e545068502cde80dc5c2a9ae
Your Name <you@example.com>
2024-02-20 15:46:00 +0330

    lesson four added

---
+++ 03/03-d.js
@@ -0,0 +1,23 @@
+let fs = require('fs');
+function rmdir() {
+    fs.rmdir(process.argv[2], message_err2);
+    
+}
+function message_err2(){
+    console.log("Folder Deleted");
+}
+function message_err(err) {
+    if(err){
+        if (err.code === "EPERM") {
+            rmdir();
+        }
+        else {
+            console.log("file not deleted", err);
+        }
+    }
+    else {
+        console.log("File Deleted");
+    }
+
+}
+fs.unlink(process.argv[2], message_err);
