3f73fc7bd01358bd8140c3537012e95de20e2186
Your Name <you@example.com>
2024-03-05 16:56:38 +0330

    lesson four(real) added

--- 04/04-a.js
+++ 04/04-a.js
@@ -1,5 +1,5 @@
-
 let fs = require('fs');
+const { json } = require('stream/consumers');
 let command = process.argv[2];
 let name = process.argv[3];
 let arg4 = process.argv[4];
@@ -12,7 +12,14 @@ function Callback(err) {
         console.log( command,'successfull.');
     }
 }
-
+function readCallback(err , data){
+    if(err){
+        console.log('ERR: ', err);
+    }
+    else{
+        console.log(data);
+    }
+}
 function unlinkCallback(err) {
     if(err){
         if(err.code === 'EPERM'){
@@ -38,7 +45,10 @@ let commands = {
         fs.unlink(name, unlinkCallback);
     },
     copy: function(){
-        fs.copyFile(name, Callback);
+        fs.copyFile(name, arg4, Callback);
+    },
+    read: function(){
+        fs.readFile(name, 'utf8', readCallback);
     }
 }
 
